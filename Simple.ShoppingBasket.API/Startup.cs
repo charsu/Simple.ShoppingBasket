fde518bfa36a993ce7f2b0ea94004d3b167c2212
Cristian Harsu <harsuc@gmail.com>
2019-03-24 18:48:44 +0000

    add swagger to web api

--- Simple.ShoppingBasket.API/Startup.cs
+++ Simple.ShoppingBasket.API/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Options;
 using Simple.ShoppingBasket.API.Core.DataSession;
 using Simple.ShoppingBasket.API.Core.Models.Mappings;
 using Simple.ShoppingBasket.API.Helpers;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Simple.ShoppingBasket.API {
    public class Startup {
@@ -26,6 +27,11 @@ namespace Simple.ShoppingBasket.API {
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services) {
          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+         // Register the Swagger generator, defining 1 or more Swagger documents
+         services.AddSwaggerGen(c => {
+            c.SwaggerDoc("v1", new Info { Title = "Simple.ShoppingBasket.API", Version = "v1" });
+         });
 
          // we tregister it as singleton in order to overcome any issues with the storage 
          services.AddSingleton<IDataRepository, FakeDataRepository>();
@@ -42,6 +48,15 @@ namespace Simple.ShoppingBasket.API {
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
       public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+         // Enable middleware to serve generated Swagger as a JSON endpoint.
+         app.UseSwagger();
+
+         // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+         // specifying the Swagger JSON endpoint.
+         app.UseSwaggerUI(c => {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+         });
+
          if (env.IsDevelopment()) {
             app.UseDeveloperExceptionPage();
          }
